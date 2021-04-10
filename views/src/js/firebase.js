ab16ee971e051739ef65516b67e061c37e32cd70
Aseeter03ps <angel.2899pasj@gmail.com>
2021-04-09 19:27:29 -0500

    firebase object pvp

--- views/src/js/firebase.js
+++ views/src/js/firebase.js
@@ -1,6 +1,39 @@
-const dbRefObject = firebase.database().ref();
+const dbRefPvP = firebase.database().ref().child("PvP");
+
+var data =  {
+    mapa: "Desierto",
+        Usuario1: {
+            id : 4,
+            posTop: 30,
+            posRight: 30,
+            posLeft: 30,
+            posBot: 30,
+            HP: 4,
+            escudo:{
+                enable: false,
+                time : 0
+            }
+        },
+        Usuario2: {
+            id : 9,
+            posTop: 30,
+            posRight: 30,
+            posLeft: 30,
+            posBot: 30,
+            HP: 4,
+            escudo:{
+                enable: false,
+                time : 0
+            }
+        }
+    }
 
 
-dbRefObject.on('value',function(snap){
+dbRefPvP.push(
+   {data}
+)
+
+dbRefPvp.on('value',function(snap){
     console.log(snap.val())
 });
+
