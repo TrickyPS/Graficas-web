74a98f9e885c318058092fe35e7c4d2ef97abade
Pedro Ramirez <soypedro222@hotmail.com>
2021-04-09 19:32:51 -0500

    Movimiento de mesh  y rotacion.

--- views/src/js/KeyboardState.js
+++ views/src/js/KeyboardState.js
@@ -1,112 +1,96 @@
+/** @namespace */
+var THREEx	= THREEx 		|| {};
+
 /**
- * @author Lee Stemkoski
- *
- * Usage: 
- * (1) create a global variable:
- *      var keyboard = new KeyboardState();
- * (2) during main loop:
- *       keyboard.update();
- * (3) check state of keys:
- *       keyboard.down("A")    -- true for one update cycle after key is pressed
- *       keyboard.pressed("A") -- true as long as key is being pressed
- *       keyboard.up("A")      -- true for one update cycle after key is released
- * 
- *  See KeyboardState.k object data below for names of keys whose state can be polled
- */
- 
-// initialization
-KeyboardState = function()
-{	
-	// bind keyEvents
-	document.addEventListener("keydown", KeyboardState.onKeyDown, false);
-	document.addEventListener("keyup",   KeyboardState.onKeyUp,   false);	
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-KeyboardState.k = 
-{  
-    8: "backspace",  9: "tab",       13: "enter",    16: "shift", 
-    17: "ctrl",     18: "alt",       27: "esc",      32: "space",
-    33: "pageup",   34: "pagedown",  35: "end",      36: "home",
-    37: "left",     38: "up",        39: "right",    40: "down",
-    45: "insert",   46: "delete",   186: ";",       187: "=",
-    188: ",",      189: "-",        190: ".",       191: "/",
-    219: "[",      220: "\\",       221: "]",       222: "'"
-}
-
-KeyboardState.status = {};
-
-KeyboardState.keyName = function ( keyCode )
+ * - NOTE: it would be quite easy to push event-driven too
+ *   - microevent.js for events handling
+ *   - in this._onkeyChange, generate a string from the DOM event
+ *   - use this as event name
+*/
+THREEx.KeyboardState	= function()
 {
-	return ( KeyboardState.k[keyCode] != null ) ? 
-		KeyboardState.k[keyCode] : 
-		String.fromCharCode(keyCode);
+	this.keyCodes	= {};
+	this.modifiers	= {};
+	
+
+
+
+	var self	= this;
+	this._onKeyDown	= function(event){ self._onKeyChange(event, true); };
+	this._onKeyUp	= function(event){ self._onKeyChange(event, false);};
+
+
+
+	document.addEventListener("keydown", this._onKeyDown, false);
+	document.addEventListener("keyup", this._onKeyUp, false);
+}
+/**
+ * To stop listening of the keyboard events
+*/
+THREEx.KeyboardState.prototype.destroy	= function()
+{
+
+
+
+	document.removeEventListener("keydown", this._onKeyDown, false);
+	document.removeEventListener("keyup", this._onKeyUp, false);
 }
 
-KeyboardState.onKeyUp = function(event)
+THREEx.KeyboardState.MODIFIERS	= ['shift', 'ctrl', 'alt', 'meta'];
+THREEx.KeyboardState.ALIAS	= {
+	'left'		: 37,
+	'up'		: 38,
+	'right'		: 39,
+	'down'		: 40,
+	'space'		: 32,
+	'pageup'	: 33,
+	'pagedown'	: 34,
+	'tab'		: 9
+};
+
+/**
+ * to process the keyboard dom event
+*/
+THREEx.KeyboardState.prototype._onKeyChange	= function(event, pressed)
 {
-	var key = KeyboardState.keyName(event.keyCode);
-	if ( KeyboardState.status[key] )
-		KeyboardState.status[key].pressed = false;
+
+
+
+
+
+
+
+var keyCode		= event.keyCode;
+this.keyCodes[keyCode]	= pressed;
+
+
+
+this.modifiers['shift']= event.shiftKey;
+this.modifiers['ctrl']	= event.ctrlKey;
+this.modifiers['alt']	= event.altKey;
+this.modifiers['meta']	= event.metaKey;
 }
 
-KeyboardState.onKeyDown = function(event)
+/**
+* query keyboard state to know if a key is pressed of not
+*
+* @param {String} keyDesc the description of the key. format : modifiers+key e.g shift+A
+* @returns {Boolean} true if the key is pressed, false otherwise
+*/
+THREEx.KeyboardState.prototype.pressed	= function(keyDesc)
 {
-	var key = KeyboardState.keyName(event.keyCode);
-	if ( !KeyboardState.status[key] )
-		KeyboardState.status[key] = { down: false, pressed: false, up: false, updatedPreviously: false };
-}
-
-KeyboardState.prototype.update = function()
-{
-	for (var key in KeyboardState.status)
-	{
-		// insure that every keypress has "down" status exactly once
-		if ( !KeyboardState.status[key].updatedPreviously )
-		{
-			KeyboardState.status[key].down        		= true;
-			KeyboardState.status[key].pressed     		= true;
-			KeyboardState.status[key].updatedPreviously = true;
-		}
-		else // updated previously
-		{
-			KeyboardState.status[key].down = false;
-		}
-
-		// key has been flagged as "up" since last update
-		if ( KeyboardState.status[key].up ) 
-		{
-			delete KeyboardState.status[key];
-			continue; // move on to next key
-		}
-		
-		if ( !KeyboardState.status[key].pressed ) // key released
-			KeyboardState.status[key].up = true;
+var keys	= keyDesc.split("+");
+for(var i = 0; i < keys.length; i++){
+	var key		= keys[i];
+	var pressed;
+	if( THREEx.KeyboardState.MODIFIERS.indexOf( key ) !== -1 ){
+		pressed	= this.modifiers[key];
+	}else if( Object.keys(THREEx.KeyboardState.ALIAS).indexOf( key ) != -1 ){
+		pressed	= this.keyCodes[ THREEx.KeyboardState.ALIAS[key] ];
+	}else {
+		pressed	= this.keyCodes[key.toUpperCase().charCodeAt(0)]
 	}
+	if( !pressed)	return false;
+};
+return true;
 }
-
-KeyboardState.prototype.down = function(keyName)
-{
-	return (KeyboardState.status[keyName] && KeyboardState.status[keyName].down);
-}
-
-KeyboardState.prototype.pressed = function(keyName)
-{
-	return (KeyboardState.status[keyName] && KeyboardState.status[keyName].pressed);
-}
-
-KeyboardState.prototype.up = function(keyName)
-{
-	return (KeyboardState.status[keyName] && KeyboardState.status[keyName].up);
-}
-
-KeyboardState.prototype.debug = function()
-{
-	var list = "Keys active: ";
-	for (var arg in KeyboardState.status)
-		list += " " + arg
-	console.log(list);
-}
-
-///////////////////////////////////////////////////////////////////////////////
